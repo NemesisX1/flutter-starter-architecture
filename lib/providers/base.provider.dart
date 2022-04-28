f8edcb5ec930efa92b9824f1517fc503931d5e4a
Junior Medehou <junior.medehou00@gmail.com>
2022-04-28 07:29:37 +0100

    Update base.provider.dart

--- lib/providers/base.provider.dart
+++ lib/providers/base.provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 /// Abstract classes for providers
-/// I call providers class with aim to work with data which are not related
+/// I call providers classes with aim to work with data which are not related
 /// to any specific viewmodel such as things like theme manager (for light/dark
 /// mode purposes maybe)
 /// You don't use them with a BaseView or a WideScopeBaseView
